939c7e21ee9e921d7c0783ace14643732167aa09
Artyom Pavlovets <pavlovetsnt@gmail.com>
2025-07-29 18:59:13 +0300

    make hw1

---
+++ python/HW1.py
@@ -0,0 +1,6 @@
+first_number = int(input('Введите первое число:')) #получение данных
+second_number = int(input('Введите второе число:'))
+sum_numbers = first_number + second_number
+print(f'Сумма чисел: {sum_numbers}')
+my_str = 'I love Python'
+print(my_str)

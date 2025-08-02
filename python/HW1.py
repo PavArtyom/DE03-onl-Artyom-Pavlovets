ba8d77fc0865f4fc9914efcb0333ad153c1f9325
Artyom Pavlovets <pavlovetsnt@gmail.com>
2025-08-02 15:45:53 +0300

    added empty lines and some comments

--- python/HW1.py
+++ python/HW1.py
@@ -1,6 +1,9 @@
-first_number = int(input('Введите первое число:')) #получение данных
-second_number = int(input('Введите второе число:'))
-sum_numbers = first_number + second_number
-print(f'Сумма чисел: {sum_numbers}')
-my_str = 'I love Python'
-print(my_str)
+# task1
+first_number = int(input('Введите первое число:')) # получение ввода с клавиатуры первого числа
+second_number = int(input('Введите второе число:')) # получение ввода с клавиатуры второго числа
+sum_numbers = first_number + second_number # вычисляем сумму введенных чисел
+print(f'Сумма чисел: {sum_numbers}') # выводим полученную сумму на экран
+
+# task2
+my_str = 'I love Python' # создаем переменную строкового типа
+print(my_str) # выводим на экран строку

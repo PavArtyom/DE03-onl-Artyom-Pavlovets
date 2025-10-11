e428bc29aea7104d44dfd8e71592fda4a8fec38e
Artyom Pavlovets <pavlovetsnt@gmail.com>
2025-10-11 15:02:25 +0300

    fix

--- sql/lesson6/lesson6.sql
+++ sql/lesson6/lesson6.sql
@@ -25,23 +25,34 @@ FROM del_cust;
 /* Добавить новую запись в таблицу rental для любого фильма категории Action, 
  при этом арендатором должен быть клиент с наибольшим количеством аренд в истории, используя вставку с подзапросом. */
 
-WITH calc_rents AS (SELECT c.customer_id, count(r.customer_id) AS total_number
+WITH
+	calc_rents AS (
+SELECT c.customer_id, count(r.customer_id) AS total_number
 FROM customer c
 JOIN rental r ON c.customer_id = r.customer_id
 GROUP BY c.customer_id
-ORDER BY total_number DESC)
+ORDER BY total_number DESC),
+	calc_inventory AS (
+SELECT i.inventory_id
+FROM inventory i
+JOIN film_category fc ON i.film_id = fc.film_id
+JOIN category c ON c.category_id = fc.category_id
+WHERE lower(c.name) = 'action')
+
+
 
 
 INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id)
 VALUES
 (
     (now()),
-    93,
+    (SELECT inventory_id FROM calc_inventory LIMIT 1),
     (SELECT customer_id FROM calc_rents LIMIT 1),
     now(),
     1
 )
 RETURNING *;
+
 
 
 
